0a59adc20dbe4577c9b2b5b5670b80d8e94ef4e1
Hao Cui <decubingexpert@gmail.com>
2025-01-28 21:54:54 -0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Layout } from 'lucide-react'
 
+
 const ActionButton = ({ to, primary, children }) => (
   <Link 
     to={to}
