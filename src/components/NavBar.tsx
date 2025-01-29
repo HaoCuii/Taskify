d93c624d501c59e40b0ba4c23cbd9165ff17da00
HaoCuii <decubingexpert@gmail.com>
2025-01-28 23:03:11 -0800

    env?

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Layout } from 'lucide-react'
 
-
 const ActionButton = ({ to, primary, children }) => (
   <Link 
     to={to}
