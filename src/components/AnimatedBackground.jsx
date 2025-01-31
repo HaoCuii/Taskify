f174d2c2710c67f9d2aff267d874264e57f4134b
Hao Cui <decubingexpert@gmail.com>
2025-01-31 14:59:23 -0800

    Update AnimatedBackground.jsx

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -144,7 +144,7 @@ const AnimatedBackground = () => {
   return (
     <canvas
       ref={canvasRef}
-      className="fixed top-0 left-0 w-full h-full z-10"
+      className="fixed top-0 left-0 w-full h-full -z-10"
     />
   );
 };
